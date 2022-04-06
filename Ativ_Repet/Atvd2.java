b8de308918d3ffdee322483e8ad2dcfb686bc46d
GyovanneMS <98404327+GyovanneMS@users.noreply.github.com>
2022-04-06 07:34:49 -0300

    Add files via upload

---
+++ Ativ_Repet/Atvd2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Atvd2{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        String login = "Gumball";
+        int senha = 12345678;
+
+        System.out.println("Olá usuário, informe o seu nome ");
+        
+
+    }
+}
--- Ativ_Repet/Atvd3.java
+++ Ativ_Repet/Atvd3.java
@@ -1,3 +1,10 @@
-public class Atvd3 {
-    
+import java.util.Scanner;
+
+class Atvd1{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+
+        System.out.print("");
+    }
 }
