04bf3234c063e0dc4ff9901e254ccb604d7a1ccd
GyovanneMS <98404327+GyovanneMS@users.noreply.github.com>
2022-04-11 11:12:48 -0300

    Add files via upload

--- Ativ_Repet/Atvd10.java
+++ Ativ_Repet/Atvd10.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 class Atvd10{
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
+        int n = 0;
 
+        while(true){
+            System.out.print("Escolha um numero: ");
+            int n = ler.nextInt();
 
-        System.out.print("Escolha um numero: ");
-        int n = ler.nextInt();
-
+            if(n >= 2){
+                System.out.println("Invalido, repita");
+                break;
+            }
+        }
+        
         for(int i=2; i<=n/2; i++){
             if(n % i ==0){
                 System.out.print("Nao eh primo");
@@ -15,5 +22,6 @@ class Atvd10{
             }
         }
         System.out.print("Eh primo.");
+        break;
     }
 }
